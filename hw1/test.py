bc1a37f6ec49ec168be88c5065f69f5206d96617
rhitabrat <pokharelrhitabrat@gmail.com>
2022-04-25 23:19:06 -0700

    files uploaded

---
+++ hw1/test.py
@@ -0,0 +1,4 @@
+import random
+
+if random.randint(1, 10) != 1:
+    print("asds")
---
+++ prog1/main.py
@@ -0,0 +1,216 @@
+class Node:
+    def __init__(self,data,level,fval):
+        """ 
+        Initialize the node with the data, level of the node and the calculated fvalue
+        """
+
+        self.data = data
+        self.level = level
+        self.fval = fval
+
+    def generate_child(self):
+        """ 
+        Generate child nodes from the given node by moving the blank space
+        either in the four directions {up,down,left,right} 
+        """
+
+        x,y = self.find(self.data,'_')
+
+        """ 
+        val_list contains position values for moving the blank space in either of
+        the 4 directions [up,down,left,right] respectively. 
+        """
+
+        val_list = [[x,y-1],[x,y+1],[x-1,y],[x+1,y]]
+        children = []
+        for i in val_list:
+            child = self.shuffle(self.data,x,y,i[0],i[1])
+            if child is not None:
+                child_node = Node(child,self.level+1,0)
+                children.append(child_node)
+        return children
+        
+    def shuffle(self,puz,x1,y1,x2,y2):
+        """ 
+        Move the blank space in the given direction and if the 
+        position value are out of limits then return None 
+        """
+        if x2 >= 0 and x2 < len(self.data) and y2 >= 0 and y2 < len(self.data):
+            temp_puz = []
+            temp_puz = self.copy(puz)
+            temp = temp_puz[x2][y2]
+            temp_puz[x2][y2] = temp_puz[x1][y1]
+            temp_puz[x1][y1] = temp
+            return temp_puz
+        else:
+            return None
+            
+
+    def copy(self,root):
+        """ 
+        Copy function to create a similar matrix of the given node
+        """
+        temp = []
+        for i in root:
+            t = []
+            for j in i:
+                t.append(j)
+            temp.append(t)
+        return temp    
+            
+    def find(self,puz,x):
+        """ 
+        Specifically used to find the position of the blank space
+        """
+        for i in range(0,len(self.data)):
+            for j in range(0,len(self.data)):
+                if puz[i][j] == x:
+                    return i,j
+
+
+class Puzzle:
+    def __init__(self,size):
+        """ 
+        Initialize the puzzle size by the specified size,open and closed lists to empty 
+        """
+        self.n = size
+        self.open = []
+        self.closed = []
+
+    def accept(self):
+        """ 
+        Accepts the puzzle from the user 
+        """
+        puz = []
+        for i in range(0,self.n):
+            temp = input().split(" ")
+            puz.append(temp)
+            
+        return puz
+
+    def f(self,start,goal):
+        """ 
+        Calcultates the value of f(x) as f(x) = h(x) + g(x) 
+        """
+        return self.h(start.data,goal)+start.level
+        # return self.h(start.data,goal)
+
+    def index_2d(self, myList, v):
+        """
+        Finds the index of the given item in the 2d list
+        """
+        for i, x in enumerate(myList):
+            if v in x:
+                return (i, x.index(v))
+
+    def h(self,start,goal):
+        """
+        Calculates the heuristics
+        """
+
+        # heuristics: number of misplaced tiles
+        temp = 0
+        for i in range(0,self.n):
+            for j in range(0,self.n):
+                if start[i][j] != goal[i][j] and start[i][j] != '_':
+                    
+                    # diff = abs(self.index_2d(start[i][j])[0] - self.index_2d(goal[i][j])[0]) + abs(self.index_2d(start[i][j])[1] - self.index_2d(goal[i][j])[1])
+                    # temp += diff
+
+                    temp += 1
+        return temp
+
+        # # heuristics: manhattan distance
+        # temp = 0
+        # for i in range(0,self.n):
+        #     for j in range(0,self.n):
+        #         if start[i][j] != goal[i][j] and start[i][j] != '_':
+                    
+        #             diff = abs(self.index_2d(start, start[i][j])[0] - self.index_2d(goal, start[i][j])[0]) +\
+        #                 abs(self.index_2d(start, start[i][j])[1] - self.index_2d(goal, start[i][j])[1])
+        #             temp += diff
+        
+        # return temp
+
+        # # heuristics: custom
+        # temp = 0
+        # for i in range(0,self.n):
+        #     for j in range(0,self.n):
+        #         if start[i][j] != goal[i][j] and start[i][j] != '_':
+        #             if goal[i][j] == '_':
+        #                 diff = int(start[i][j])
+        #             else:
+        #                 diff = abs(int(start[i][j]) - int(goal[i][j]))
+        #             temp += diff
+        
+        # return temp
+        
+
+    def process(self):
+        """ Accept Start and Goal Puzzle state"""
+        # print("Enter the start state matrix \n")
+        # start = self.accept()
+
+        # # 8-puzzle
+        # start = [['1', '2', '3'], ['4', '8', '5'], ['7', '6', '_']]
+        # start = [['1', '2', '3'], ['7', '4', '5'], ['8', '6', '_']]
+        # start = [['1', '2', '8'], ['4', '5', '7'], ['3', '6', '_']]
+        # start = [['1', '2', '3'], ['4', '5', '7'], ['8', '6', '_']]
+        # start = [['1', '3', '2'], ['4', '5', '6'], ['7', '8', '_']]
+        
+        # 15-puzzle
+        start = [['1', '2', '3', '4'], ['5', '6', '7', '8'], ['9', '10', '11', '12'], ['13', '15', '14', '_']]
+        # start = [['1', '2', '3', '4'], ['5', '6', '7', '8'], ['9', '10', '11', '12'], ['14', '13', '15', '_']]
+        # start = [['1', '2', '3', '4'], ['5', '6', '7', '8'], ['9', '10', '11', '12'], ['15', '14', '13', '_']]
+        # start = [['1', '2', '3', '4'], ['5', '6', '7', '8'], ['9', '10', '12', '11'], ['13', '14', '15', '_']]
+        # start = [['1', '2', '3', '4'], ['5', '6', '7', '8'], ['9', '10', '11', '15'], ['13', '14', '12', '_']]
+
+        # print("Enter the goal state matrix \n")
+        # goal = self.accept()
+        goal = [['1', '2', '3', '4'], ['5', '6', '7', '8'], ['9', '10', '11', '12'], ['13', '14', '15', '_']]
+
+        start = Node(start,0,0)
+        # start.fval = self.f(start,goal)
+        # print(start.fval)
+
+        # put the start node in the open list
+        self.open.append(start)
+
+        # steps counter
+        steps = -1
+
+        while True:
+            steps+=1
+            # if steps == 1000:
+                # break
+
+            cur = self.open[0]
+
+            # for i in cur.data:
+            #     for j in i:
+            #         print(j,end=" ")
+            # print()
+
+            # print("\nHeuristic value:", self.h(cur.data,goal))
+            if(self.h(cur.data,goal) == 0):
+                break
+
+            for i in cur.generate_child():
+                i.fval = self.f(i,goal)
+                # print(i.fval)
+                self.open.append(i)
+            self.closed.append(cur)
+            del self.open[0]
+
+            # sort the list based on f value
+            self.open.sort(key = lambda x:x.fval,reverse=False)
+        
+        print("Number of steps taken: ", steps)
+
+
+puz = Puzzle(4)
+puz.process()
+
+
+
+# Resources used: https://gist.github.com/Ajinkya-Sonawane/e04bd6cc43014e24bee74e2a9a0dca18#file-npuzzle-py
